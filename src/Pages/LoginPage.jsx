fe051f65e5abce75e370b48a4247f3aededd8bb5
Bindumuniyappa <muniyappabindu7@gmail.com>
2023-08-28 11:27:33 +0530

    made changes to login page

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState,useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom'; 
 import axios from 'axios';
 import "../styling/LoginStyle.css";
@@ -54,6 +54,10 @@ const LoginPage = () => {
       }
     }
   };
+
+   useEffect(()=>{
+
+    },[])
   
 
   return ( 
